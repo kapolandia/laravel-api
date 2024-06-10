d8b28000341731e99fae348ab2efcd0e6d4608dc
Kapo <paykapo4@gmail.com>
2024-06-10 16:15:39 +0200

    add project api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::with('type', 'technologies')->get();
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
+}
